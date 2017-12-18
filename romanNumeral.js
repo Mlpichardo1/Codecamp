de6d5012973de402dbf402c846a49fe7d5a46be6
Manuel Pichardo <mlpichardo1@gmail.com>
2017-12-18 22:52:58 +0000

    Added psuedocode

---
+++ romanNumeral.js
@@ -0,0 +1,15 @@
+// input- any given number, 
+
+// output- answers should be uppercase, should be letters in Roman numeral form
+
+
+// solution example:
+// 1  identify a number
+// 2  array or an object that stores roman numerals
+// 3  match number to its roman counterpart
+// 4  for loop the romans numerals matches
+function convertToRoman(num) {
+ return num;
+}
+
+convertToRoman(36);
