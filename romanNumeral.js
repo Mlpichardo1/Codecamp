0cdebe017e8330ed50c1368047f6e9215bd69324
Manuel Pichardo <mlpichardo1@gmail.com>
2017-12-18 23:45:19 +0000

    roman numeral complete

--- romanNumeral.js
+++ romanNumeral.js
@@ -9,7 +9,18 @@
 // 3  match number to its roman counterpart
 // 4  for loop the romans numerals matches
 function convertToRoman(num) {
- return num;
+  var value = [ 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 ];
+  var romanNum = [ 'M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I' ];
+  var romanized = '';
+  
+  for (var index = 0; index < value.length; index++) {
+    while (value[index] <= num) {
+      romanized += romanNum[index];
+      num -= value[index];
+    }
+  }
+
+  return romanized;
 }
 
 convertToRoman(36);
